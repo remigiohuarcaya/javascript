f42a485d33e8e1ff9f1338f38d6b3e46d817fa33
Remigio Huarcaya <rshuarcaya@gmail.com>
2023-11-24 09:53:32 -0500

    Versión de clase 24-11-2023

---
+++ js/s30.js
@@ -0,0 +1,10 @@
+function saludar(){
+    alert("Buenos días");
+}    
+function mensaje(){
+    alert("La humildad es parte del exito personal");
+}
+function mostrar(){
+    var emp = document.getElementById('txtEmpresa').value;
+    alert(emp);
+}
